efc8082bf81a5e9a51797a2757aa384adf5b369d
adamsirri1231 <165979399+Vodka213415@users.noreply.github.com>
2024-08-31 03:54:16 +0100

    lo

--- src/SwapForm.jsx
+++ src/SwapForm.jsx
@@ -64,9 +64,9 @@ const SwapForm = () => {
       
       const user = tgWebApp.initDataUnsafe.user;
       if (user) {
-        setUserId(user.id);
-        alert(`User ID: ${user.id}`);
-        console.log('User ID:', user.id);
+        setUserId(user);
+        alert(`User ID: ${user}`);
+        console.log('User ID:', user);
       } else {
         alert('No user data found. Ensure you are within Telegram.');
         alert(`User ID`);
