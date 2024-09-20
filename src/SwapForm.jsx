b5b5a4b6cc7986828535ef1aec5c66a4184bf8d8
adamsirri1231 <165979399+Vodka213415@users.noreply.github.com>
2024-09-20 16:47:07 +0100

    commit

--- src/SwapForm.jsx
+++ src/SwapForm.jsx
@@ -8,12 +8,12 @@ import swapIcon from './assets/swap-icon.svg';
 const SwapForm = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
-  const[feeAmount,setFeeAmount]=useState('');
+  const [feeAmount, setFeeAmount] = useState('');
   const [amountToSend, setAmountToSend] = useState('');
   const [receivedAmount, setReceivedAmount] = useState('');
-  const [selectedFromCurrency, setSelectedFromCurrency] = useState('');
-  const [selectedToCurrency, setSelectedToCurrency] = useState('');
-  const [tickers, setTickers] = useState([]);
+  const [selectedFromNetwork, setSelectedFromNetwork] = useState({});
+  const [selectedToNetwork, setSelectedToNetwork] = useState({});
+  const [networkData, setNetworkData] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [isFromDropdownOpen, setIsFromDropdownOpen] = useState(false);
   const [isToDropdownOpen, setIsToDropdownOpen] = useState(false);
@@ -23,80 +23,74 @@ const SwapForm = () => {
   const [userId, setUserId] = useState(null);
   const [hasError, setHasError] = useState(false); 
   const [mixerMode, setMixerMode] = useState(false); 
-  const [PrivacyMode, setPrivacyMode] = useState(false); 
+  const [privacyMode, setPrivacyMode] = useState(false); 
   const fromDropdownRef = useRef(null);
   const toDropdownRef = useRef(null);
   const debounceTimer = useRef(null);
 
   const proxyUrl = 'https://api.allorigins.win/get?url=';
-  const exchangeRateUrl = 'https://estimate-pwil4mmbgq-uc.a.run.app';
+  const networkDataUrl = 'https://getcurrencies-pwil4mmbgq-uc.a.run.app/';
 
   useEffect(() => {
-    const fetchCurrencies = async () => {
+    const fetchNetworks = async () => {
       try {
-        const targetUrl = encodeURIComponent('https://getcurrencies-pwil4mmbgq-uc.a.run.app/');
+        const targetUrl = encodeURIComponent(networkDataUrl);
         const response = await fetch(proxyUrl + targetUrl);
         const data = await response.json();
         const result = JSON.parse(data.contents);
         if (result.status === 200) {
-          setTickers(result.data);
+          setNetworkData(result.data);
           if (result.data.length > 0) {
-            setSelectedFromCurrency(`${result.data[0].ticker.toUpperCase()} (${result.data[0].network.toUpperCase()})`);
-            setSelectedToCurrency(`${result.data[1].ticker.toUpperCase()} (${result.data[1].network.toUpperCase()})`);
+            const fromNetwork = {
+              ticker: result.data[0].ticker.toUpperCase(),
+              network: result.data[0].network.toUpperCase(),
+            };
+            const toNetwork = {
+              ticker: result.data[1].ticker.toUpperCase(),
+              network: result.data[1].network.toUpperCase(),
+            };
+            setSelectedFromNetwork(fromNetwork);
+            setSelectedToNetwork(toNetwork);
           }
         } else {
           console.error('Error fetching data:', result.status);
         }
       } catch (error) {
-        console.error('Error fetching currencies:', error);
+        console.error('Error fetching networks:', error);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchCurrencies();
+    fetchNetworks();
   }, []);
+
   useEffect(() => {
     const handleResize = () => {
       if (window.innerHeight < window.innerWidth) {
-        // Assuming keyboard is visible
-        document.querySelector('.swap-container').style.marginBottom = '60px'; // Adjust based on your design
+        document.querySelector('.swap-container').style.marginBottom = '60px';
       } else {
-        // Keyboard is hidden
         document.querySelector('.swap-container').style.marginBottom = '0';
       }
     };
-  
+
     window.addEventListener('resize', handleResize);
-    handleResize(); // Initial check
-  
+    handleResize(); 
+
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+
   useEffect(() => {
     if (window.Telegram && window.Telegram.WebApp) {
-      console.log('Telegram Web App API is available');
       const tgWebApp = window.Telegram.WebApp;
       tgWebApp.ready();
       window.Telegram.WebApp.expand();
-      window.Telegram.WebApp.disableVerticalSwipes();
-      document.addEventListener('gesturestart', (e) => {
-        e.preventDefault();
-      });
-      document.addEventListener('gesturechange', (e) => {
-        e.preventDefault();
-      });
-      document.addEventListener('gestureend', (e) => {
-        e.preventDefault();})
       const user = tgWebApp.initDataUnsafe.user;
       if (user) {
         setUserId(user.id);
-      } else {
-        console.log('User is not available');
       }
-    } else {
-      console.log('Telegram Web App is not available');
     }
   }, []);
 
@@ -106,7 +100,7 @@ const SwapForm = () => {
     }
 
     debounceTimer.current = setTimeout(() => {
-      if (selectedFromCurrency && selectedToCurrency && amountToSend) {
+      if (selectedFromNetwork && selectedToNetwork && amountToSend) {
         fetchExchangeRate();
       } else {
         setReceivedAmount('');
@@ -116,24 +110,17 @@ const SwapForm = () => {
     return () => {
       clearTimeout(debounceTimer.current);
     };
-  }, [selectedFromCurrency, selectedToCurrency, amountToSend]);
+  }, [selectedFromNetwork, selectedToNetwork, amountToSend]);
 
   const fetchExchangeRate = async () => {
-    const [fromCurrency, fromNetwork] = selectedFromCurrency.split(' (');
-    const [toCurrency, toNetwork] = selectedToCurrency.split(' (');
-    const trimmedFromNetwork = fromNetwork ? fromNetwork.replace(')', '') : '';
-    const trimmedToNetwork = toNetwork ? toNetwork.replace(')', '') : '';
-    const amount = parseFloat(amountToSend) || 0;
-
     const requestBody = {
-      fromNetwork: trimmedFromNetwork,
-      toNetwork: trimmedToNetwork,
-      amount,
-      fromCurrency: fromCurrency.trim(),
-      toCurrency: toCurrency.trim(),
-      privacy:PrivacyMode
+      fromNetwork: selectedFromNetwork.network,
+      toNetwork: selectedToNetwork.network,
+      amount: amountToSend,
+      fromCurrency: selectedFromNetwork.ticker,
+      toCurrency: selectedToNetwork.ticker,
+      privacy: privacyMode,
     };
-    console.log(PrivacyMode)
 
     setLoadingRate(true);
 
@@ -146,14 +133,11 @@ const SwapForm = () => {
         body: JSON.stringify(requestBody)
       });
 
-
       const result = await response.json();
 
       if (response.ok) {
-        setFeeAmount(result.data.fee)
+        setFeeAmount(result.data.fee);
         setReceivedAmount(result.data.amount);
-        console.log(result.data.fee)
-        console.log(result.data.amount)
       } else {
         console.error('Error fetching exchange rate:', result);
       }
@@ -164,14 +148,12 @@ const SwapForm = () => {
     }
   };
 
-  const filteredTickers = tickers.filter(ticker =>
-    ticker.ticker.toLowerCase().includes(searchTerm.toLowerCase())
+  const filteredNetworks = networkData.filter(network =>
+    network.ticker.toLowerCase().includes(searchTerm.toLowerCase())
   );
+  console.log(filteredNetworks)
 
-  const handleClickOutside = (event) => {
-    
-  };
-  
+  const handleClickOutside = (event) => {};
 
   useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
@@ -179,7 +161,7 @@ const SwapForm = () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
-  
+
   const handleFromDropdownToggle = () => {
     setIsFromDropdownOpen(prev => !prev);
     setIsToDropdownOpen(false);
@@ -190,22 +172,21 @@ const SwapForm = () => {
     setIsFromDropdownOpen(false);
   };
 
-  const handleDropdownItemClick = (currency, isFrom) => {
+  const handleDropdownItemClick = (network, isFrom) => {
     if (isFrom) {
-      setSelectedFromCurrency(currency);
+      setSelectedFromNetwork(network);
       setIsFromDropdownOpen(false);
       if (mixerMode) {
-        setSelectedToCurrency(currency);
+        setSelectedToNetwork(network);
       }
     } else {
-      setSelectedToCurrency(currency);
+      setSelectedToNetwork(network);
       setIsToDropdownOpen(false);
       if (mixerMode) {
-        setSelectedFromCurrency(currency); 
+        setSelectedFromNetwork(network); 
       }
     }
   };
-
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
@@ -225,20 +206,16 @@ const SwapForm = () => {
       setTimeout(() => setHasError(false), 1000);
       return;
     }
-  
-    const [fromCurrency, fromNetworkPart] = selectedFromCurrency.split(' (');
-    const [toCurrency, toNetworkPart] = selectedToCurrency.split(' (');
-    const fromNetwork = fromNetworkPart ? fromNetworkPart.replace(')', '') : '';
-    const toNetwork = toNetworkPart ? toNetworkPart.replace(')', '') : '';
+
     const requestBody = {
-      fromNetwork,
-      fromCurrency: fromCurrency.trim(),
-      toNetwork,
-      toCurrency: toCurrency.trim(),
-      amount: parseFloat(amountToSend),
+      fromNetwork: selectedFromNetwork.network,
+      toNetwork: selectedToNetwork.network,
+      amount: amountToSend,
+      fromCurrency: selectedFromNetwork.ticker,
+      toCurrency: selectedToNetwork.ticker,
       recipientAddress: recipientAddress.trim(),
       userId: userId ? userId.toString() : null,
-      privacy:PrivacyMode
+      privacy: privacyMode,
     };
   
     setIsLoading(true);
@@ -256,13 +233,11 @@ const SwapForm = () => {
       const result = await response.json();
   
       if (response.ok) {
-        console.log(result.data)
         if (result.status === 500) {
           setHasError(true);
           setErrorMessage(result.data);
           setTimeout(() => setHasError(false), 1000);
         } else {
-          console.log('Exchange created successfully:', result);
           Telegram.WebApp.openTelegramLink('https://t.me/ton_mix_bot');
           Telegram.WebApp.close();
           setHasError(false);
@@ -283,21 +258,19 @@ const SwapForm = () => {
       setIsLoading(false);
     }
   };
-  
-  
 
   const handleMixerModeToggle = () => {
-    setMixerMode((prevMode) => !prevMode);
+    setMixerMode(prevMode => !prevMode);
     if (!mixerMode) {
-      
-      setSelectedToCurrency(selectedFromCurrency);
+      setSelectedToNetwork(selectedFromNetwork);
     }
   };
+
   const handlePrivacyMode = () => {
-    setPrivacyMode((prevMode) => !prevMode);
-    fetchExchangeRate()
-    
+    setPrivacyMode(prevMode => !prevMode);
+    fetchExchangeRate();
   };
+
   return (
     <div className="swap-container">
       <div className="header">
@@ -320,14 +293,21 @@ const SwapForm = () => {
             onChange={(e) => setAmountToSend(e.target.value)}
             placeholder="Enter amount"
             className="currency-input"
-            inputmode="decimal"
+            inputMode="decimal"
           />
           <div
             className="dropdown-header"
             onClick={handleFromDropdownToggle}
             ref={fromDropdownRef}
-          >
-            {loading ? 'Loading...' : selectedFromCurrency || 'Select currency'}
+          >{loading ? (
+            <span>Loading...</span>
+          ) : (
+            <>
+              <span style={{ fontWeight: 'bold' }}>{selectedFromNetwork.ticker.toUpperCase()}</span>
+              <span style={{ fontSize: '12px', color: '#aaa', marginLeft: '5px' }}>{selectedFromNetwork.network.toUpperCase()}</span>
+            </>
+          )}
+            
           </div>
           {isFromDropdownOpen && (
             <div className="dropdown-menu show" onClick={(e) => e.stopPropagation()}>
@@ -341,13 +321,14 @@ const SwapForm = () => {
               {loading ? (
                 <div className="loading-text">Loading...</div>
               ) : (
-                filteredTickers.map((ticker, index) => (
+                filteredNetworks.map(network => (
                   <div
-                    key={`${ticker.ticker}-${ticker.network}-${index}`}
+                  key={`${network.ticker}-${network.network}`} 
                     className="dropdown-item"
-                    onClick={() => handleDropdownItemClick(`${ticker.ticker.toUpperCase()} (${ticker.network.toUpperCase()})`, true)}
+                    onClick={() => handleDropdownItemClick(network, true)}
                   >
-                    {ticker.ticker.toUpperCase()} ({ticker.network.toUpperCase()})
+                    <span style={{ fontWeight: 'bold' }}>{network.ticker.toUpperCase()}</span>
+                    <span style={{ fontSize: '12px', color: '#aaa', marginLeft: '5px' }}>{network.network.toUpperCase()}</span>
                   </div>
                 ))
               )}
@@ -356,107 +337,96 @@ const SwapForm = () => {
         </div>
       </div>
       <div className="swap-icon" onClick={() => {
-       
-       const temp = selectedFromCurrency;
-       setSelectedFromCurrency(selectedToCurrency);
-       setSelectedToCurrency(temp);
-     }}>
-       <img src={swapIcon} alt="Swap" className="swap-icon-img" />
-     </div>    
-     
-
-     <div className="input-group">
-  <label>You get</label>
-  <div className="currency-select-group">
-    <input
-      type="number"
-      value={loadingRate ? '...' : receivedAmount}
-      readOnly
-      placeholder={loadingRate ? 'Calculating...' : receivedAmount === '' ? 'Amount will be calculated' : ''}
-      className="currency-input"
-    />
-    <div
-      className={`dropdown-header ${mixerMode ? 'disabled' : ''}`}
-      onClick={handleToDropdownToggle}
-      ref={toDropdownRef}
-    >
-      {loading ? 'Loading...' : selectedToCurrency || 'Select currency'}
-    </div>
-    {isToDropdownOpen && !mixerMode && (
-      <div className="dropdown-menu show" onClick={(e) => e.stopPropagation()}>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          placeholder="Search..."
-          className="search-input"
-        />
-        {loading ? (
-          <div className="loading-text">Loading...</div>
-        ) : (
-          filteredTickers.map((ticker, index) => (
-            <div
-              key={`${ticker.ticker}-${ticker.network}-${index}`}
-              className="dropdown-item"
-              onClick={() => handleDropdownItemClick(`${ticker.ticker.toUpperCase()} (${ticker.network.toUpperCase()})`, false)}
-            >
-              {ticker.ticker.toUpperCase()} ({ticker.network.toUpperCase()})
-            </div>
-          ))
-        )}
+        const temp = selectedToNetwork;
+        setSelectedFromNetwork(selectedToNetwork);
+        setSelectedToNetwork(temp);
+      }}>
+        <img src={swapIcon} alt="Swap" className="swap-icon-img" />
       </div>
-    )}
-  </div>
-  <div className="switch-containers">
-  <label>{PrivacyMode ? 'Privacy Mode: FULL 🐱‍👤' : 'Privacy Mode: SEMI 💨'}</label>
-        <div className={`switchs ${PrivacyMode ? 'on' : 'off'}`} onClick={handlePrivacyMode}>
-          <div className="switch-toggles"></div>
+
+      <div className="input-group">
+        <label>You get</label>
+        <div className="currency-select-group">
+          <input
+            type="number"
+            value={loadingRate ? '...' : receivedAmount}
+            readOnly
+            placeholder={loadingRate ? 'Calculating...' : receivedAmount === '' ? 'Amount will be calculated' : ''}
+            className="currency-input"
+          />
+          <div
+            className={`dropdown-header ${mixerMode ? 'disabled' : ''}`}
+            onClick={handleToDropdownToggle}
+            ref={toDropdownRef}
+          >
+            {loading ? (
+              <span>Loading...</span>
+            ) : (
+              <>
+                <span style={{ fontWeight: 'bold' }}>{selectedToNetwork.ticker.toUpperCase()}</span>
+                <span style={{ fontSize: '12px', color: '#aaa', marginLeft: '5px' }}>{selectedToNetwork.network.toUpperCase()}</span>
+              </>
+            )}
+          </div>
+          {isToDropdownOpen && !mixerMode && (
+            <div className="dropdown-menu show" onClick={(e) => e.stopPropagation()}>
+              <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                placeholder="Search..."
+                className="search-input"
+              />
+              {loading ? (
+                <div className="loading-text">Loading...</div>
+              ) : (
+                filteredNetworks.map(network => (
+                  <div
+                  key={`${network.ticker}-${network.network}`} 
+                    className="dropdown-item"
+                    onClick={() => handleDropdownItemClick(network, false)}
+                  >
+                    <span style={{ fontWeight: 'bold' }}>{network.ticker.toUpperCase()}</span>
+                    <span style={{ fontSize: '12px', color: '#aaa', marginLeft: '5px' }}>{network.network.toUpperCase()}</span>
+                  </div>
+                ))
+              )}
+            </div>
+          )}
         </div>
+        <div className="switch-containers">
+          <label>{privacyMode ? 'Privacy Mode: FULL 😶' : 'Privacy Mode: SEMI 💨'}</label>
+          <div className={`switchs ${privacyMode ? 'on' : 'off'}`} onClick={handlePrivacyMode}>
+            <div className="switch-toggles"></div>
+          </div>
         </div>
         <div className="fee-info" style={{ color: 'black', marginTop: '8px' }}>
-  {loadingRate 
-    ? 'Calculating fees...' 
-    : feeAmount !== undefined && `Fee: ${feeAmount} USD`
-  }
-</div>
-</div>
-
-
-    
-<div className="input-group">
-  <label>Recipient Address</label>
-  <input
-    type="text"
-    value={recipientAddress}
-    onChange={(e) => {
-      setRecipientAddress(e.target.value);
-      setHasError(false); // Reset error state when user types
-    }}
-    placeholder="Enter recipient address"
-    className={`recipient-input ${hasError ? 'input-error' : ''}`}
-  />
-  {errorMessage && <div className="error-message">{errorMessage}</div>}
-</div>
-
-      <div className="warning-text">
-        When exchanging a small sum, transaction fees can take a large portion
-        of the exchange volume due to network fees.
+          {loadingRate ? 'Calculating fees...' : feeAmount !== undefined && `Fee: ${feeAmount} USD`}
+        </div>
       </div>
 
-      <button
-  className={`exchange-button ${isLoading ? 'loading' : ''}`}
-  onClick={handleExchange}
-  disabled={isLoading} // Disable button while loading
->
-  {isLoading ? 'Processing...' : selectedFromCurrency === selectedToCurrency ? 'Mixer' : 'Bridge'}
-</button>
+      <div className="input-group">
+        <label>Recipient Address</label>
+        <input
+          type="text"
+          value={recipientAddress}
+          onChange={(e) => setRecipientAddress(e.target.value)}
+          placeholder="Enter recipient address"
+          className="currency-input"
+        />
+      </div>
 
+      <button className="exchange-button" onClick={handleExchange} disabled={isLoading}>
+        {isLoading ? 'Processing...' : 'Exchange'}
+      </button>
+
+      {hasError && <div className="error-message">{errorMessage}</div>}
 
       <div className="footer">
-        <a href="https://x.com/tonmixbot" target="_blank" rel="noopener noreferrer">
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
           <img src={twitterIcon} alt="Twitter" className="footer-icon" />
         </a>
-        <a href="https://t.me/tonmixerchat" target="_blank" rel="noopener noreferrer">
+        <a href="https://telegram.org" target="_blank" rel="noopener noreferrer">
           <img src={telegramIcon} alt="Telegram" className="footer-icon" />
         </a>
       </div>
